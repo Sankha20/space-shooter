26bbef878ad45fb3d9a50390ab5484e7dce22a7e
Victor Pontes <=>
2018-10-06 18:45:41 -0300

    Moved controls code to separate file

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ function main(p) {
 
     Game.loadGame();
     loadUIManager();
+    loadControls(p);
 
     spawnEnemy = () => {
         if (Game.timer == 120) {
@@ -25,68 +26,5 @@ function main(p) {
     p.draw = () => {
         drawUI();
         manageKeys();
-    }
-
-    // INTERACTION
-    const manageKeys = () => {
-        // W 87
-        if (keys[87]) {
-            player.moveUp();
-        }
-
-        // S 83
-        if (keys[83]) {
-            player.moveDown();
-        }
-
-        // A 65
-        if (keys[65]) {
-            player.moveLeft();
-        }
-
-        // D 68
-        if (keys[68]) {
-            player.moveRight();
-        }
-    }
-
-    // MOUSE
-
-    p.mouseClicked = () => {
-        Game.visibleButtons.forEach(btn => {
-            if (btn.isUnderMouse()) {
-                return btn.handleClick();
-            }
-        })
-    }
-    p.mouseMoved = () => {
-        let underMouse = false;
-        for (var i = 0; i < Game.visibleButtons.length; i++) {
-            let btn = Game.visibleButtons[i];
-
-            if (btn.isUnderMouse()) {
-                underMouse = true;
-                break;
-            }
-        }
-
-        if (underMouse) {
-            p.cursor(p.HAND);
-        } else {
-            p.cursor(p.ARROW);
-        }
-    }
-
-    // Keyboard
-    p.keyPressed = () => {
-        let key = p.keyCode;
-
-        keys[key] = true;
-    }
-
-    p.keyReleased = () => {
-        let key = p.keyCode;
-
-        keys[key] = false;
     }
 }
---
+++ js/player-controls.js
@@ -0,0 +1,64 @@
+loadControls = (p) => {
+    
+    manageKeys = () => {
+        // W 87
+        if (keys[87]) {
+            player.moveUp();
+        }
+
+        // S 83
+        if (keys[83]) {
+            player.moveDown();
+        }
+
+        // A 65
+        if (keys[65]) {
+            player.moveLeft();
+        }
+
+        // D 68
+        if (keys[68]) {
+            player.moveRight();
+        }
+    }
+
+    // MOUSE
+
+    p.mouseClicked = () => {
+        Game.visibleButtons.forEach(btn => {
+            if (btn.isUnderMouse()) {
+                return btn.handleClick();
+            }
+        })
+    }
+    p.mouseMoved = () => {
+        let underMouse = false;
+        for (var i = 0; i < Game.visibleButtons.length; i++) {
+            let btn = Game.visibleButtons[i];
+
+            if (btn.isUnderMouse()) {
+                underMouse = true;
+                break;
+            }
+        }
+
+        if (underMouse) {
+            p.cursor(p.HAND);
+        } else {
+            p.cursor(p.ARROW);
+        }
+    }
+
+    // Keyboard
+    p.keyPressed = () => {
+        let key = p.keyCode;
+
+        keys[key] = true;
+    }
+
+    p.keyReleased = () => {
+        let key = p.keyCode;
+
+        keys[key] = false;
+    }
+}
